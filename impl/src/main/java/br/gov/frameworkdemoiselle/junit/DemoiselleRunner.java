f9cd23dedb94d2807783109e52fb5e4be527b7a8
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-22 14:53:17 -0300

    Experimentando carregamento com isolamento de ClassLoader

--- impl/src/main/java/br/gov/frameworkdemoiselle/junit/DemoiselleRunner.java
+++ impl/src/main/java/br/gov/frameworkdemoiselle/junit/DemoiselleRunner.java
@@ -36,7 +36,8 @@
  */
 package br.gov.frameworkdemoiselle.junit;
 
-import org.jboss.weld.environment.se.StartMain;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
@@ -44,66 +45,152 @@ import org.junit.runners.model.InitializationError;
 
 import br.gov.frameworkdemoiselle.internal.bootstrap.BeforeApplicationFinalization;
 import br.gov.frameworkdemoiselle.internal.bootstrap.BeforeApplicationInitialization;
-import br.gov.frameworkdemoiselle.util.Beans;
 
 public class DemoiselleRunner extends BlockJUnit4ClassRunner {
 
-	private static boolean containerInitialized = false;
+	private Weld weld;
 
-	private static Integer count = 0;
+	private WeldContainer container;
+
+	private ClassLoader classLoader;
+
+	// private static boolean containerInitialized = false;
+
+	// private static Integer count = 0;
+
+	private int count;
 
 	public DemoiselleRunner(Class<?> testClass) throws InitializationError {
-		super(testClass);
+		super(getFromTestClassloader(testClass));
+
+		this.classLoader = getTestClass().getJavaClass().getClassLoader();
+
 		init();
 	}
 
-	private synchronized void init() {
-		if (!containerInitialized) {
-			StartMain.main(null);
-			containerInitialized = true;
-
-			startup();
+	private static Class<?> getFromTestClassloader(Class<?> clazz) throws InitializationError {
+		try {
+			ClassLoader c = new TestClassLoader();
+			// ClassLoader classLoader = new URLClassLoader();
+			return Class.forName(clazz.getName(), true, c);
+		} catch (ClassNotFoundException e) {
+			throw new InitializationError(e);
 		}
 	}
 
-	@Override
-	protected void finalize() throws Throwable {
-		shutdown();
-		super.finalize();
+	@SuppressWarnings("unchecked")
+	private/* synchronized */void init() {
+		// if (!containerInitialized) {
+
+		// this.classLoader = new TestClassLoader();
+		Class<Weld> type;
+
+		try {
+			type = (Class<Weld>) Class.forName(Weld.class.getName(), true, this.classLoader);
+			this.weld = type.newInstance();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// this.weld = new Weld();
+		this.container = weld.initialize();
+		this.count = this.testCount();
+
+		// Beans.setBeanManager(container.getBeanManager());
+
+		// StartMain.main(null);
+		// containerInitialized = true;
+
+		startup();
+		// }
+
+		// count += testCount();
 	}
+
+	// @Override
+	// protected void finalize() throws Throwable {
+	// shutdown();
+	// super.finalize();
+	// }
 
 	@Override
 	protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+		// Beans.setBeanManager(container.getBeanManager());
 		super.runChild(method, notifier);
 
-		synchronized (count) {
-			if (++count == this.testCount()) {
-				shutdown();
-			}
+		// count--;
+		// count--;
+
+		if (--count == 0) {
+			// if (count == 0) {
+			shutdown();
+			// container.f
+			// } else {
+			// while (count != 0) {
+			// try {
+			// Thread.currentThread().sleep(1000);
+			// } catch (InterruptedException e) {
+			// // TODO Auto-generated catch block
+			// e.printStackTrace();
+			// }
+			// }
+			// }
 		}
 	}
 
+	@Override
+	public void run(RunNotifier notifier) {
+		super.run(notifier);
+		weld.shutdown();
+		// container.instance().s
+	}
+
+	// @SuppressWarnings("unused")
 	protected Object createTest() throws Exception {
-		return Beans.getReference(getTestClass().getJavaClass());
+		// Beans.setBeanManager(container.getBeanManager());
+		return container.instance().select(getTestClass().getJavaClass()).get();
+		//
+		// return Beans.getReference(getTestClass().getJavaClass());
 	}
 
 	private void startup() {
-		Beans.getBeanManager().fireEvent(new BeforeApplicationInitialization() {
+		// Beans.setBeanManager(container.getBeanManager());
+		container.event().select(BeforeApplicationInitialization.class).fire(new BeforeApplicationInitialization() {
 
 			@Override
 			public boolean removeProcessors() {
-				return false;
+				return true;
 			}
 		});
+
+		// Beans.getBeanManager().fireEvent(new BeforeApplicationInitialization() {
+		//
+		// @Override
+		// public boolean removeProcessors() {
+		// return false;
+		// }
+		// });
 	}
 
 	private void shutdown() {
-		Beans.getBeanManager().fireEvent(new BeforeApplicationFinalization() {
+		// Beans.setBeanManager(container.getBeanManager());
+		container.event().select(BeforeApplicationFinalization.class).fire(new BeforeApplicationFinalization() {
 
 			@Override
 			public boolean removeProcessors() {
-				return false;
+				return true;
 			}
 		});
+
+		// Beans.getBeanManager().fireEvent(new BeforeApplicationFinalization() {
+		//
+		// @Override
+		// public boolean removeProcessors() {
+		// return false;
+		// }
+		// });
 	}
+
 }
